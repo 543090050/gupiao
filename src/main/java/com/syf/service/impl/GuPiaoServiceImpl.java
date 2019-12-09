8af063cf240134c8be29258e3f430b4720239cb4
shiyufeng <shiyff@yonyou.com>
2019-12-09 16:06:46 +0800

    123

--- src/main/java/com/syf/service/impl/GuPiaoServiceImpl.java
+++ src/main/java/com/syf/service/impl/GuPiaoServiceImpl.java
@@ -2,31 +2,23 @@ package com.syf.service.impl;
 
 import com.syf.domain.GuPiao;
 import com.syf.service.IGuPiaoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class GuPiaoServiceImpl implements IGuPiaoService {
 
-    @Autowired
-    private NamedParameterJdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;//Spring的JdbcTemplate是自动配置的，可直接使用
 
     @Override
     public void create(GuPiao guPiao) {
-        String sql = "insert into t_author(id,real_name,nick_name) " +
-                "values(:id,:realName,:nickName)";
-        Map<String, Object> param = new HashMap<>();
-        param.put("id",guPiao.getId());
-        param.put("name", guPiao.getName());
+        jdbcTemplate.update("insert into USER(NAME, AGE) values(?, ?)",
+                guPiao.getId(), guPiao.getName());
     }
 
     @Override
     public void delete(String id) {
-
+        jdbcTemplate.update("delete from USER where id = ?", id);
     }
 
     @Override
